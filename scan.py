9575de6ae5d67de7073d12fb9754331c6ec22ffd
eidetech <marcuseide@me.com>
2021-03-04 22:50:44 +0100

    Added scanDay function

---
+++ scan.py
@@ -0,0 +1,77 @@
+import pynput
+from pynput.mouse import Button, Controller
+from time import sleep
+import RGBreader
+mouse = Controller()
+
+#DEBUG show mouse pos:
+#print(mouse.position)
+
+def scanDay(day):
+    isBooked = {} # Define return variable
+    if (day == "Monday"):
+        # Check if selected room is booked next Tuesday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Tuesday"):
+        # Check if selected room is booked next Wednesday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Wednesday"):
+        # Check if selected room is booked next Thursday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Thursday"):
+        # Check if selected room is booked next Friday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Friday"):
+        # Check if selected room is booked next Saturday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Saturday"):
+        # Check if selected room is booked next Sunday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    elif(day == "Sunday"):
+        # Check if selected room is booked next Monday
+        x = 1167
+        y = 745
+        xPos = x
+        #DEBUG move mouse pos to start:
+        #mouse.position=(x,y)
+    else:
+        return "The argument provided is not a day."
+        
+    while(x<(xPos+164)):
+        if (RGBreader.checkIfRoomIsBooked(x, y) == True):
+            isBooked = True
+            break
+        else:
+            isBooked = False
+
+        #DEBUG show mouse pos on screen:
+        #mouse.position=(x,y)
+        x=x+10
+        sleep(0.1)
+    return isBooked
+
+print(scanDay("Saturday"))
