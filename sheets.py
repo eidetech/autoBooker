6a9082e3a3e73d5aff94f4c0ba9c96fed9d1769f
eidetech <marcuseide@me.com>
2021-03-11 22:06:15 +0100

    Fixed weekend row bug

--- sheets.py
+++ sheets.py
@@ -10,22 +10,33 @@ def insertToSheets(room, time):
     worksheet = sh.worksheet("2021")
     backend = sh.worksheet("backend")
 
-    # Get the row to put the booked room details into from backend sheet
-    todaysRow = backend.acell('A2').value
-    # Generate the next row number
-    nextRow = int(todaysRow)+1
-    # Update the row value in backend sheet to nextRow
-    backend.update('A2', (nextRow))
-
     # New entry data
     # room = "C3 090"
     # time = "08:30 - 11:30"
 
-    # Skip values for weekends
+    # Get the row to put the booked room details into from backend sheet
+    todaysRow = backend.acell('A2').value
+
+    # Return all row numbers that are weekend days:
     weekendDays = backend.col_values(2)
     if todaysRow in weekendDays:
+        # If todaysRow is in the list of rows that are weekend days:
         print("todaysRow is in weekendDays: skipping weekend.")
         todaysRow = int(todaysRow)+2
+        print("Todays row is a weekend day: " + str(todaysRow))
+        nextRow = int(todaysRow)+1
+        print("Next row: " + str(nextRow))
+        # Update the row value in backend sheet to nextRow
+        backend.update('A2', (nextRow))
+        # Insert booking data to worksheet
+    else:
+        # If the current row is a weekday:
+        print("todaysRow is not in weekendDays, proceeding:")
+        # Generate the next row number
+        nextRow = int(todaysRow)+1
+        print("Next row: " + str(nextRow))
+        # Update the row value in backend sheet to nextRow
+        backend.update('A2', (nextRow))
 
-    # Insert booking data to worksheet
+
     worksheet.update("C"+str(todaysRow)+":D"+str(todaysRow), [[room, time]])
